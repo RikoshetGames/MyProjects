2fa4f12925566d7193c7c6cde29b2a3a30ead9a9
RikoshetGames <ilyalew@mail.ru>
2023-08-28 21:43:45 +0300

    Python Project

--- Trenagers/main.py
+++
@@ -1,9 +0,0 @@
-from pin import check_pin
-
-user_input = input("Введите ваш Пин-код: ")
-
-if check_pin(user_input):
-    print("Такой пин подходит")
-
-else:
-    print("Не подходит")
--- Trenagers/validators/validate_pin.py
+++
@@ -1,5 +0,0 @@
-def validate_pin(pin):
-    if len(pin) == 4 and pin.isdigit():
-        return True
-    else:
-        return False
