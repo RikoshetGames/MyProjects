5f538994f87b645d5bda9399b9c2533aed9777a9
RikoshetGames <ilyalew@mail.ru>
2023-08-29 23:29:27 +0300

    Python Project

--- CourseWork2/main.py
+++ CourseWork2/main.py
@@ -4,7 +4,7 @@ from utils import load_random_word, game, hello
 
 
 def main():
-    name = input("Введите имя игрока: ") # Получаем имя игрока
+    name = input("Введите имя игрока: ")  # Получаем имя игрока
     player = Player(name)  # Создаем экземпляр класса игрока
     random_word = load_random_word("https://jsonkeeper.com/b/LJHI")  # Загружаем случайное слово
     hello(player, random_word)
