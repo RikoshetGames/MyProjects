aa039f3aa2f65aa747aedd0fc26312ef08fad2d6
RikoshetGames <ilyalew@mail.ru>
2023-09-02 17:50:55 +0300

    Python Project

--- CourseWork2/utils.py
+++ CourseWork2/utils.py
@@ -5,22 +5,31 @@ import requests
 import urllib3
 
 
-def load_random_word(url):
+def load_json_keeper(url):
     """
-    Получит список слов с внешнего ресурса,
-    выберет случайное слово,
-    создаст экземпляр класса `BasicWord`,
-    вернет этот экземпляр.
-    :param url: Адрес внешнего ресурса.
-    :return: Экземпляр класса.
+    Получит список слов с внешнего ресурса.
+    :param url: Ссылка на внешний ресурс
+    :return: Выведенные данные
     """
-    urllib3.disable_warnings() # Отключаем предупреждения о безопасности SSL-сертификата
+    urllib3.disable_warnings()  # Отключаем предупреждения о безопасности SSL-сертификата
     response = requests.get(url=url, verify=False) # Загружаем данные из внешнего ресурса
     random_words = response.json() # Преобразуем данные в формат JSON в список
-    if response.status_code == 200:
-        random_word = random.choice(random_words) # Выбираем случайное слово из списка
-        instance = BasicWord(random_word["word"], random_word["subwords"]) # Создаем экземпляр класса BasicWord
-        return instance
+    if response.status_code == 200: # делаем проверку
+        return random_words
+    return None
+
+
+def choice_word(random_words):
+    """
+    Выберет случайное слово,
+    создаст экземпляр класса `BasicWord`,
+    вернет этот экземпляр.
+    :param random_words: Полученные данные источника.
+    :return: Экземпляр класса.
+    """
+    random_word = random.choice(random_words)  # Выбираем случайное слово из списка
+    instance = BasicWord(random_word["word"], random_word["subwords"]) # Создаем экземпляр класса BasicWord
+    return instance
 
 
 def hello_player(player, random_word):
