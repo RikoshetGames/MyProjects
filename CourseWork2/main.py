b3c5b49c406b77684370253e93519c0a24dc7aef
RikoshetGames <ilyalew@mail.ru>
2023-09-02 18:14:37 +0300

    Python Project

--- CourseWork2/main.py
+++ CourseWork2/main.py
@@ -6,11 +6,10 @@ from utils import load_json_keeper, choice_word, start_game, hello_player
 def main():
     name = input("Введите имя игрока: ")  # Получаем имя игрока
     player = Player(name)  # Создаем экземпляр класса игрока
-    # random_word = load_random_word("https://jsonkeeper.com/b/LJHI")
     load_word = load_json_keeper("https://jsonkeeper.com/b/LJHI") # Загружаем случайное слово
     random_word = choice_word(load_word) # получаем экземпляр класса BasicWord
-    hello_player(player, random_word)
-    start_game(random_word, player)
+    hello_player(player, random_word) # приветствуем игрока
+    start_game(random_word, player) # запускаем игру
 
 if __name__ == "__main__":
     main()
